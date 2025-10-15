5179677fcf55f54cbc18a8b354cabedb745ae462
phil0452 <70770291+phil0452@users.noreply.github.com>
2025-10-15 10:31:10 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,107 @@ import Cart from './components/cart/Cart.jsx'
 import data from './store/data.json'
 
 function App() {
+  const content = [
+    {
+       "image": {
+            "thumbnail": "./assets/images/image-waffle-thumbnail.jpg",
+            "mobile": "./assets/images/image-waffle-mobile.jpg",
+            "tablet": "./assets/images/image-waffle-tablet.jpg",
+            "desktop": "./assets/images/image-waffle-desktop.jpg"
+       },
+       "name": "Waffle with Berries",
+       "category": "Waffle",
+       "price": 6.50
+    },
+    {
+        "image": {
+            "thumbnail": "./assets/images/image-creme-brulee-thumbnail.jpg",
+            "mobile": "./assets/images/image-creme-brulee-mobile.jpg",
+            "tablet": "./assets/images/image-creme-brulee-tablet.jpg",
+            "desktop": "./assets/images/image-creme-brulee-desktop.jpg"
+        },
+        "name": "Vanilla Bean Crème Brûlée",
+        "category": "Crème Brûlée",
+        "price": 7.00
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-macaron-thumbnail.jpg",
+            "mobile": "./assets/images/image-macaron-mobile.jpg",
+            "tablet": "./assets/images/image-macaron-tablet.jpg",
+            "desktop": "./assets/images/image-macaron-desktop.jpg"
+        },
+        "name": "Macaron Mix of Five",
+        "category": "Macaron",
+        "price": 8.00
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-tiramisu-thumbnail.jpg",
+            "mobile": "./assets/images/image-tiramisu-mobile.jpg",
+            "tablet": "./assets/images/image-tiramisu-tablet.jpg",
+            "desktop": "./assets/images/image-tiramisu-desktop.jpg"
+        },
+        "name": "Classic Tiramisu",
+        "category": "Tiramisu",
+        "price": 5.50
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-baklava-thumbnail.jpg",
+            "mobile": "./assets/images/image-baklava-mobile.jpg",
+            "tablet": "./assets/images/image-baklava-tablet.jpg",
+            "desktop": "./assets/images/image-baklava-desktop.jpg"
+        },
+        "name": "Pistachio Baklava",
+        "category": "Baklava",
+        "price": 4.00
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-meringue-thumbnail.jpg",
+            "mobile": "./assets/images/image-meringue-mobile.jpg",
+            "tablet": "./assets/images/image-meringue-tablet.jpg",
+            "desktop": "./assets/images/image-meringue-desktop.jpg"
+        },
+        "name": "Lemon Meringue Pie",
+        "category": "Pie",
+        "price": 5.00
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-cake-thumbnail.jpg",
+            "mobile": "./assets/images/image-cake-mobile.jpg",
+            "tablet": "./assets/images/image-cake-tablet.jpg",
+            "desktop": "./assets/images/image-cake-desktop.jpg"
+        },
+        "name": "Red Velvet Cake",
+        "category": "Cake",
+        "price": 4.50
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-brownie-thumbnail.jpg",
+            "mobile": "./assets/images/image-brownie-mobile.jpg",
+            "tablet": "./assets/images/image-brownie-tablet.jpg",
+            "desktop": "./assets/images/image-brownie-desktop.jpg"
+        },
+        "name": "Salted Caramel Brownie",
+        "category": "Brownie",
+        "price": 4.50
+     },
+     {
+        "image": {
+            "thumbnail": "./assets/images/image-panna-cotta-thumbnail.jpg",
+            "mobile": "./assets/images/image-panna-cotta-mobile.jpg",
+            "tablet": "./assets/images/image-panna-cotta-tablet.jpg",
+            "desktop": "./assets/images/image-panna-cotta-desktop.jpg"
+        },
+        "name": "Vanilla Panna Cotta",
+        "category": "Panna Cotta",
+        "price": 6.50
+     }
+];
   const dessert = [];
   return (
     <>
@@ -45,29 +146,11 @@ function App() {
             <h1 className="text_color_rose_900">Desserts</h1>
 
             <div id="desset_menu" className="flex">            
-              {data.map((dessert) => <DessertItem dessertInfo={dessert} /> )}
+              {content.map((dessert) => <DessertItem dessertInfo={dessert} /> )}
             </div>
           </div>
 
-          <Cart />
-          {/* <div id="confirm_order_modal" className="modal hidden">
-            <form id="confirm_order_form">
-              <img src="./assets/images/icon-order-confirmed.svg" alt="icon order confirmed" />
-              <h1>Order Confirmed</h1>
-              <p className="text_color_rose_500 font_size_productnames">We hope you enjoy you food!</p>
-
-              <div  className="cart_items bg_color_rose_50">        
-                <div id="cartitems">
-                </div>
-                <div id="order_total_container" className="order_total item_flex">
-                  <span className="font_weight_400 order_total_span">Order Total</span>
-                  <span id="checkout_total" className="font_weight_700"></span>
-                </div>
-              </div>
-
-              <button id="confirm_order" className="qty_container bg_color_red text_color_white font_weight_600">Start New Order</button>
-            </form>
-          </div> */}
+          <Cart />         
         </section>        
       </DessertProvider>
     </>
