4d4b0c046296fb2117be0becd179fe22d941bc73
phil0452 <70770291+phil0452@users.noreply.github.com>
2025-10-15 14:09:45 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"]
+  content: ["./index.html", "./src/**/*.{js,jsx}"]
 })
