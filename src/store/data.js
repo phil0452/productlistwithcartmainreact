59bd8ff47624d2d09f1c86ddce0fa89c335f0b24
phil0452 <70770291+phil0452@users.noreply.github.com>
2025-10-17 08:14:09 -0700

    Add files via upload

--- src/store/data.js
+++ src/store/data.js
@@ -1,102 +1,148 @@
+import WaffleThumbnail from "/src/assets/images/image-waffle-thumbnail.jpg";
+import WaffleMobile from "/src/assets/images/image-waffle-mobile.jpg";
+import Waffletablet from "/src/assets/images/image-waffle-tablet.jpg";
+import Waffledesktop from "/src/assets/images/image-waffle-desktop.jpg";
+
+import Bruleethumbnail from "/src/assets/images/image-creme-brulee-thumbnail.jpg";
+import Bruleemobile from "/src/assets/images/image-creme-brulee-mobile.jpg";
+import  Bruleetablet from "/src/assets/images/image-creme-brulee-tablet.jpg";
+import  Bruleedesktop from "/src/assets/images/image-creme-brulee-desktop.jpg";
+
+import Macaronthumbnail from "/src/assets/images/image-macaron-thumbnail.jpg";
+import Macaronmobile from "/src/assets/images/image-macaron-mobile.jpg";
+import Macarontablet from "/src/assets/images/image-macaron-tablet.jpg";
+import Macarondesktop from "/src/assets/images/image-macaron-desktop.jpg";
+
+import Tiramisuthumbnail from "/src/assets/images/image-tiramisu-thumbnail.jpg";
+import Tiramisumobile from "/src/assets/images/image-tiramisu-mobile.jpg";
+import Tiramisutablet from "/src/assets/images/image-tiramisu-tablet.jpg";
+import Tiramisudesktop from "/src/assets/images/image-tiramisu-desktop.jpg";
+
+import Baklavathumbnail from "/src/assets/images/image-baklava-thumbnail.jpg";
+import Baklavamobile from "/src/assets/images/image-baklava-mobile.jpg";
+import Baklavatablet from "/src/assets/images/image-baklava-tablet.jpg";
+import Baklavadesktop from "/src/assets/images/image-baklava-desktop.jpg";
+
+import Meringuethumbnail from "/src/assets/images/image-meringue-thumbnail.jpg";
+import Meringuemobile from "/src/assets/images/image-meringue-mobile.jpg";
+import Meringuetablet from "/src/assets/images/image-meringue-tablet.jpg";
+import Meringuedesktop from "/src/assets/images/image-meringue-desktop.jpg";
+
+import Cakethumbnail from "/src/assets/images/image-cake-thumbnail.jpg";
+import Cakemobile from "/src/assets/images/image-cake-mobile.jpg";
+import Caketablet from "/src/assets/images/image-cake-tablet.jpg";
+import Cakedesktop from "/src/assets/images/image-cake-desktop.jpg";
+
+import Browniethumbnail from "/src/assets/images/image-brownie-thumbnail.jpg";
+import Browniemobile from "/src/assets/images/image-brownie-mobile.jpg";
+import Brownietablet from "/src/assets/images/image-brownie-tablet.jpg";
+import Browniedesktop from "/src/assets/images/image-brownie-desktop.jpg";
+
+import PannaCottathumbnail from "/src/assets/images/image-panna-cotta-thumbnail.jpg";
+import PannaCottamobile from "/src/assets/images/image-panna-cotta-mobile.jpg";
+import PannaCottatablet from "/src/assets/images/image-panna-cotta-tablet.jpg";
+import PannaCottadesktop from "/src/assets/images/image-panna-cotta-desktop.jpg";
+      
+
 const data = [
     {
-       "image": {
-            "thumbnail": "./assets/images/image-waffle-thumbnail.jpg",
-            "mobile": "./assets/images/image-waffle-mobile.jpg",
-            "tablet": "./assets/images/image-waffle-tablet.jpg",
-            "desktop": "./assets/images/image-waffle-desktop.jpg"
+        image: {
+            thumbnail: WaffleThumbnail,
+            mobile: WaffleMobile,
+            tablet: Waffletablet,
+            desktop: Waffledesktop
        },
-       "name": "Waffle with Berries",
-       "category": "Waffle",
-       "price": 6.50
+       name: "Waffle with Berries",
+       category: "Waffle",
+       price: 6.50
     },
     {
-        "image": {
-            "thumbnail": "./assets/images/image-creme-brulee-thumbnail.jpg",
-            "mobile": "./assets/images/image-creme-brulee-mobile.jpg",
-            "tablet": "./assets/images/image-creme-brulee-tablet.jpg",
-            "desktop": "./assets/images/image-creme-brulee-desktop.jpg"
+         image: {
+            thumbnail: Bruleethumbnail,
+            mobile: Bruleemobile,
+            tablet: Bruleetablet,
+            desktop: Bruleedesktop
         },
-        "name": "Vanilla Bean Crème Brûlée",
-        "category": "Crème Brûlée",
-        "price": 7.00
+        name: "Vanilla Bean Crème Brûlée",
+        category: "Crème Brûlée",
+        price: 7.00
      },
      {
-        "image": {
-            "thumbnail": "./assets/images/image-macaron-thumbnail.jpg",
-            "mobile": "./assets/images/image-macaron-mobile.jpg",
-            "tablet": "./assets/images/image-macaron-tablet.jpg",
-            "desktop": "./assets/images/image-macaron-desktop.jpg"
+         image: {
+            thumbnail: Macaronthumbnail,
+            mobile: Macaronmobile,
+            tablet: Macarontablet,
+            desktop: Macarondesktop
         },
-        "name": "Macaron Mix of Five",
-        "category": "Macaron",
-        "price": 8.00
+        name: "Macaron Mix of Five",
+        category: "Macaron",
+        price: 8.00
      },
      {
-        "image": {
-            "thumbnail": "./assets/images/image-tiramisu-thumbnail.jpg",
-            "mobile": "./assets/images/image-tiramisu-mobile.jpg",
-            "tablet": "./assets/images/image-tiramisu-tablet.jpg",
-            "desktop": "./assets/images/image-tiramisu-desktop.jpg"
+         image: {
+            thumbnail: Tiramisuthumbnail,
+            mobile: Tiramisumobile,
+            tablet: Tiramisutablet,
+            desktop: Tiramisudesktop
         },
-        "name": "Classic Tiramisu",
-        "category": "Tiramisu",
-        "price": 5.50
+        name: "Classic Tiramisu",
+        category: "Tiramisu",
+        price: 5.50
      },
      {
-        "image": {
-            "thumbnail": "./assets/images/image-baklava-thumbnail.jpg",
-            "mobile": "./assets/images/image-baklava-mobile.jpg",
-            "tablet": "./assets/images/image-baklava-tablet.jpg",
-            "desktop": "./assets/images/image-baklava-desktop.jpg"
+         image: {
+            thumbnail: Baklavathumbnail,
+            mobile: Baklavamobile,
+            tablet: Baklavatablet,
+            desktop: Baklavadesktop
         },
-        "name": "Pistachio Baklava",
-        "category": "Baklava",
-        "price": 4.00
+        name: "Pistachio Baklava",
+        category: "Baklava",
+        price: 4.00
      },
      {
-        "image": {
-            "thumbnail": "./assets/images/image-meringue-thumbnail.jpg",
-            "mobile": "./assets/images/image-meringue-mobile.jpg",
-            "tablet": "./assets/images/image-meringue-tablet.jpg",
-            "desktop": "./assets/images/image-meringue-desktop.jpg"
+         image: {
+            thumbnail: Meringuethumbnail,
+            mobile: Meringuemobile,
+            tablet: Meringuetablet,
+            desktop: Meringuedesktop
         },
-        "name": "Lemon Meringue Pie",
-        "category": "Pie",
-        "price": 5.00
+        name: "Lemon Meringue Pie",
+        category: "Pie",
+        price: 5.00
      },
      {
-        "image": {
-            "thumbnail": "./assets/images/image-cake-thumbnail.jpg",
-            "mobile": "./assets/images/image-cake-mobile.jpg",
-            "tablet": "./assets/images/image-cake-tablet.jpg",
-            "desktop": "./assets/images/image-cake-desktop.jpg"
+         image: {
+            thumbnail: Cakethumbnail,
+            mobile: Cakemobile,
+            tablet: Caketablet,
+            desktop: Cakedesktop
         },
-        "name": "Red Velvet Cake",
-        "category": "Cake",
-        "price": 4.50
+        name: "Red Velvet Cake",
+        category: "Cake",
+        price: 4.50
      },
      {
-        "image": {
-            "thumbnail": "./assets/images/image-brownie-thumbnail.jpg",
-            "mobile": "./assets/images/image-brownie-mobile.jpg",
-            "tablet": "./assets/images/image-brownie-tablet.jpg",
-            "desktop": "./assets/images/image-brownie-desktop.jpg"
+         image: {
+            thumbnail: Browniethumbnail,
+            mobile: Browniemobile,
+            tablet: Brownietablet,
+            desktop: Browniedesktop
         },
-        "name": "Salted Caramel Brownie",
-        "category": "Brownie",
-        "price": 4.50
+        name: "Salted Caramel Brownie",
+        category: "Brownie",
+        price: 4.50
      },
      {
-        "image": {
-            "thumbnail": "./assets/images/image-panna-cotta-thumbnail.jpg",
-            "mobile": "./assets/images/image-panna-cotta-mobile.jpg",
-            "tablet": "./assets/images/image-panna-cotta-tablet.jpg",
-            "desktop": "./assets/images/image-panna-cotta-desktop.jpg"
+         image: {
+            thumbnail: PannaCottathumbnail,
+            mobile: PannaCottamobile,
+            tablet: PannaCottatablet,
+            desktop: PannaCottadesktop
         },
-        "name": "Vanilla Panna Cotta",
-        "category": "Panna Cotta",
-        "price": 6.50
+        name: "Vanilla Panna Cotta",
+        category: "Panna Cotta",
+        price: 6.50
      }
 ]
 
