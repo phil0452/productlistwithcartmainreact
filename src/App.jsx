0c0a5c8f13b55e333048102cf303be791f4dad8a
phil0452 <70770291+phil0452@users.noreply.github.com>
2025-10-17 08:16:19 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,118 +3,18 @@ import { useState } from 'react'
 import { DessertProvider } from "/src/store/DessertProvider";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import './main.css'
+import './App.css' //for a file in the same directory
+import './main.css' //for a file in the same directory
 // import './components/dessert/dessertItem.css'
 
 //import Items from './components/dessert/Items.jsx'
-import DessertItem from './components/dessert/DessertItem.jsx'
-import Cart from './components/cart/Cart.jsx'
+import DessertItem from '/src/components/dessert/DessertItem.jsx' //when the file path is absolute (here, must start with src)
+import Cart from '/src/components/cart/Cart.jsx' //when the file path is absolute (here, must start with src)
 
-import data from './store/data.json'
+// import data from './store/data.json'
+import data from '/src/store/data.js'
 
 function App() {
-  const content = [
-    {
-       "image": {
-            "thumbnail": "./assets/images/image-waffle-thumbnail.jpg",
-            "mobile": "./assets/images/image-waffle-mobile.jpg",
-            "tablet": "./assets/images/image-waffle-tablet.jpg",
-            "desktop": "./assets/images/image-waffle-desktop.jpg"
-       },
-       "name": "Waffle with Berries",
-       "category": "Waffle",
-       "price": 6.50
-    },
-    {
-        "image": {
-            "thumbnail": "./assets/images/image-creme-brulee-thumbnail.jpg",
-            "mobile": "./assets/images/image-creme-brulee-mobile.jpg",
-            "tablet": "./assets/images/image-creme-brulee-tablet.jpg",
-            "desktop": "./assets/images/image-creme-brulee-desktop.jpg"
-        },
-        "name": "Vanilla Bean Crème Brûlée",
-        "category": "Crème Brûlée",
-        "price": 7.00
-     },
-     {
-        "image": {
-            "thumbnail": "./assets/images/image-macaron-thumbnail.jpg",
-            "mobile": "./assets/images/image-macaron-mobile.jpg",
-            "tablet": "./assets/images/image-macaron-tablet.jpg",
-            "desktop": "./assets/images/image-macaron-desktop.jpg"
-        },
-        "name": "Macaron Mix of Five",
-        "category": "Macaron",
-        "price": 8.00
-     },
-     {
-        "image": {
-            "thumbnail": "./assets/images/image-tiramisu-thumbnail.jpg",
-            "mobile": "./assets/images/image-tiramisu-mobile.jpg",
-            "tablet": "./assets/images/image-tiramisu-tablet.jpg",
-            "desktop": "./assets/images/image-tiramisu-desktop.jpg"
-        },
-        "name": "Classic Tiramisu",
-        "category": "Tiramisu",
-        "price": 5.50
-     },
-     {
-        "image": {
-            "thumbnail": "./assets/images/image-baklava-thumbnail.jpg",
-            "mobile": "./assets/images/image-baklava-mobile.jpg",
-            "tablet": "./assets/images/image-baklava-tablet.jpg",
-            "desktop": "./assets/images/image-baklava-desktop.jpg"
-        },
-        "name": "Pistachio Baklava",
-        "category": "Baklava",
-        "price": 4.00
-     },
-     {
-        "image": {
-            "thumbnail": "./assets/images/image-meringue-thumbnail.jpg",
-            "mobile": "./assets/images/image-meringue-mobile.jpg",
-            "tablet": "./assets/images/image-meringue-tablet.jpg",
-            "desktop": "./assets/images/image-meringue-desktop.jpg"
-        },
-        "name": "Lemon Meringue Pie",
-        "category": "Pie",
-        "price": 5.00
-     },
-     {
-        "image": {
-            "thumbnail": "./assets/images/image-cake-thumbnail.jpg",
-            "mobile": "./assets/images/image-cake-mobile.jpg",
-            "tablet": "./assets/images/image-cake-tablet.jpg",
-            "desktop": "./assets/images/image-cake-desktop.jpg"
-        },
-        "name": "Red Velvet Cake",
-        "category": "Cake",
-        "price": 4.50
-     },
-     {
-        "image": {
-            "thumbnail": "./assets/images/image-brownie-thumbnail.jpg",
-            "mobile": "./assets/images/image-brownie-mobile.jpg",
-            "tablet": "./assets/images/image-brownie-tablet.jpg",
-            "desktop": "./assets/images/image-brownie-desktop.jpg"
-        },
-        "name": "Salted Caramel Brownie",
-        "category": "Brownie",
-        "price": 4.50
-     },
-     {
-        "image": {
-            "thumbnail": "./assets/images/image-panna-cotta-thumbnail.jpg",
-            "mobile": "./assets/images/image-panna-cotta-mobile.jpg",
-            "tablet": "./assets/images/image-panna-cotta-tablet.jpg",
-            "desktop": "./assets/images/image-panna-cotta-desktop.jpg"
-        },
-        "name": "Vanilla Panna Cotta",
-        "category": "Panna Cotta",
-        "price": 6.50
-     }
-];
   const dessert = [];
   return (
     <>
@@ -146,7 +46,7 @@ function App() {
             <h1 className="text_color_rose_900">Desserts</h1>
 
             <div id="desset_menu" className="flex">            
-              {content.map((dessert) => <DessertItem dessertInfo={dessert} /> )}
+              {data.map((dessert) => <DessertItem dessertInfo={dessert} /> )}
             </div>
           </div>
 
